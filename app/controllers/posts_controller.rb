705ce4a63d3436ed9bb984f8538695ac5d608079
John Tuttle <thejwt@gmail.com>
2017-11-21 22:43:45 +0000

    automatically backed up by learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -13,7 +13,7 @@ class PostsController < ApplicationController
       @post.update(post_params)
       redirect_to post_path(@post)
     else
-      redirect_to edit(@post)
+      render :endit
     end
 
   end
