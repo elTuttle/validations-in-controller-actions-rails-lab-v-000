e98c63c91d8ce6a28459fdf762264ca8fe0600e2
John Tuttle <thejwt@gmail.com>
2017-11-21 22:39:50 +0000

    automatically backed up by learn

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -11,13 +11,11 @@ class AuthorsController < ApplicationController
 
     if @author.valid?
       @author.save
-
       redirect_to author_path(@author)
     else
-
       render :new
     end
-  
+
   end
 
   private
